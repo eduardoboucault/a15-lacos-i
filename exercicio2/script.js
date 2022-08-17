6f1f67775469bd0f2ac44fe49280c3b22e397f18
Eduardo Boucault <boucault.labenu@outlook.com>
2022-08-17 15:09:18 -0300

    Aula com feedback realizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,7 +1,9 @@
 let numero = Number(prompt('Ditige um número de 1 a 10:'))   //? Input do usuário com número da tabuada
 
-for(i=1;i<=10;i++){
+for( i = 1 ; i <= 10 ; i++){
+
     console.log(`${numero*i}`)
+    
 }
 
 //? Exemplo tabuada
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,7 @@
 const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
 
-for(i=0; i <=4; i++){
-    console.log(maioresPaises[i])
+for( i = 0 ; i <= maioresPaises.length-1 ; i++){
+
+    console.log(`${i+1} - ${maioresPaises[i]}`)
+    
 }
